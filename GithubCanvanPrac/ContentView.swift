330a7b6f8f28a2f934a30cff266d547a0a139840
sunshiningsoo <yenkee1219@gmail.com>
2022-05-27 23:22:10 +0900

    Initial commit

---
+++ GithubCanvanPrac/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  GithubCanvanPrac
+//
+//  Created by 박성수 on 2022/05/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ GithubCanvanPrac/GithubCanvanPracApp.swift
@@ -0,0 +1,17 @@
+//
+//  GithubCanvanPracApp.swift
+//  GithubCanvanPrac
+//
+//  Created by 박성수 on 2022/05/27.
+//
+
+import SwiftUI
+
+@main
+struct GithubCanvanPracApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
